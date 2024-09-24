e97c75cc99cad858601d6f825222b1f1bbaec82d
talhazubaerbd <lieutenant34555@gmail.com>
2024-09-25 01:47:37 +0600

    this commit

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const header= document.getElementById("navbar");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky" , window.scrollY> 0);
 });
+
+document.getElementById('donate2').addEventListener('click', function(event){
+    event.preventDefault();
+
+    let addMonney = document.getElementById('amount2').value;
+    console.log(addMonney)
+}) 
+document.getElementById('donate1').addEventListener('click', function(event){
+    event.preventDefault();
+
+    let addMonney = document.getElementById('amount1').value;
+    console.log(addMonney)
+}) 
+document.getElementById('donate3').addEventListener('click', function(event){
+    event.preventDefault();
+
+    let addMonney = document.getElementById('amount3').value;
+    console.log(addMonney)
+}) 
